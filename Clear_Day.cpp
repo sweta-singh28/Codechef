9c59802fc0354551f14ec0bba23278955c3bb556
sweta-singh28 <swetasinghgs28@gmail.com>
2024-02-19 19:04:47 +0530

    sweta

---
+++ Clear_Day.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int X, Y;
+    cin>>X;
+    cin>>Y;
+
+    int Z = 7 - X - Y;
+    cout<<Z<<endl;
+    
+    return 0;
+}
---
+++ Double_Rent.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int X;
+    cin>>X;
+        int Y = X * 2;
+        cout<<Y<<endl;
+}
