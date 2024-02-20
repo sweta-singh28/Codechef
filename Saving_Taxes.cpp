e0ebcbbeae65bc3385696aaa4cc5f5070a176a84
sweta-singh28 <swetasinghgs28@gmail.com>
2024-02-20 18:03:38 +0530

    sweta

---
+++ Saving_Taxes.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--){
+	 int X, Y;
+     cin>>X;
+     cin>>Y;
+
+    int Z = max (0, X - Y);
+    cout<<Z<<endl;
+	}
+
+}
