ebad421e2ce1643f443b95ced842a452ec404505
sweta-singh28 <swetasinghgs28@gmail.com>
2024-02-20 18:37:41 +0530

    sweta

---
+++ Masterchef_finals.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int T;
+    cin>>T;
+    
+    while(T--){
+    int X;
+    cin>>X;
+    if(X <= 10){
+        cout<<"Yes"<<endl;
+    }
+    else{
+        cout<<"No"<<endl;
+    }
+    }
+}
