14d68e2d5c66b669266b214877a6b35f325aa10d
sweta-singh28 <swetasinghgs28@gmail.com>
2024-02-22 01:36:00 +0530

    sweta

---
+++ Off_By_One.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int A, B;
+    cin>>A;
+    cin>>B;
+    int C;
+    C = A + B;
+    cout<<C<<"1"<<endl;
+}
