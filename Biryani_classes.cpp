5b423bd1c86670be32ef11aa51ad473b7184451c
sweta-singh28 <swetasinghgs28@gmail.com>
2024-02-21 11:05:24 +0530

    sweta

--- Biryani_classes.cpp
+++ Biryani_classes.cpp
@@ -1,8 +1,18 @@
-#include<iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
+int main() {
+     int T;
+     cin>>T;
+     
+     while(T--){
+         int X, Y;
+         cin>>X;
+         cin>>Y;
+         int Z;
+         Z = X * Y;
+         cout<<Z<<endl;
+         
+     }
 
-    
-    return 0;
 }
