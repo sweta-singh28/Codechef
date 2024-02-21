3b2e950c1c1f075432ecff2aecdfcf120168c156
sweta-singh28 <swetasinghgs28@gmail.com>
2024-02-21 14:42:58 +0530

    sweta

---
+++ Kitchen_Timings.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	    int X, Y;
+	    cin>>X;
+	    cin>>Y;
+	    int Z;
+	    Z = Y - X;
+	    cout<<Z<<endl;
+	}
+
+}
