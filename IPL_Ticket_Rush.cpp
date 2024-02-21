7899ed21c0bf656cedabeddef9e669c3ec3d741d
sweta-singh28 <swetasinghgs28@gmail.com>
2024-02-22 01:23:54 +0530

    sweta

---
+++ IPL_Ticket_Rush.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--){
+	    int M, N;
+	    cin>>M;
+	    cin>>N;
+	    
+	    int Z;
+	    Z = max(0, M - N);
+	    cout<<Z<<endl;
+	}
+
+}
