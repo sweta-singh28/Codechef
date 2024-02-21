4752849101947ff01ebe01bd38059d758f7a8220
sweta-singh28 <swetasinghgs28@gmail.com>
2024-02-21 20:36:29 +0530

    sweta

---
+++ Contests/Healthy_Sleep.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int H;
+	cin>>H;
+	if(H < 8){
+	    cout<<"LESS"<<endl;
+	}
+	else if(H > 8){
+	    cout<<"MORE"<<endl;
+	}
+	else{
+	    cout<<"PERFECT"<<endl;
+	}
+
+}
