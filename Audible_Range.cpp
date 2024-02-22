863d8cb349ab5b25f4969b0d958d7a459bd737b5
sweta-singh28 <swetasinghgs28@gmail.com>
2024-02-22 21:06:35 +0530

    sweta

---
+++ Audible_Range.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes her
+	
+	int T;
+	cin>>T;
+	
+	while(T--){
+	    int X;
+	    cin>>X;
+	    if(X>=67 && X<=45000){
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	    
+	}
+
+}
