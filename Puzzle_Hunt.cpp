10066113d63c346d5a7e5b1e09d0f38cb1741ae0
sweta-singh28 <swetasinghgs28@gmail.com>
2024-02-23 21:06:51 +0530

    sweta

---
+++ Puzzle_Hunt.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int n;
+    cin>>n;
+    if( n>=6 && n<=8){
+        cout<<"YES";
+    } 
+    else{
+        cout<<"NO"<<endl;
+    }
+}
