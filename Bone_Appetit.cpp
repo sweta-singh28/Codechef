91fd6621a27bd81145550bfaf07096313c68288f
sweta-singh28 <swetasinghgs28@gmail.com>
2024-02-24 19:31:57 +0530

    sweta

---
+++ Bone_Appetit.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n, m, x, y, z, c, sum;
+	cin>>n, m;
+	cin>>x, y;
+	 
+	z = n * x;
+	c = m * y;
+	
+	sum = z + c;
+	cout<<sum<<endl;
+	
+
+}
