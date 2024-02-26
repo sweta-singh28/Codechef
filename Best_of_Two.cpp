d450e2dccc88d8a52faef2aa57ab4a0b8d9fdbc6
sweta-singh28 <swetasinghgs28@gmail.com>
2024-02-26 10:07:43 +0530

    sweta

---
+++ Best_of_Two.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    int t;
+    cin>>t;
+    while(t--){
+        int x, y;
+        cin>>x>>y;
+        if(x>y){
+            cout<<x<<endl;
+        }
+        else{
+            cout<<y<<endl;
+        }
+    }
+}
