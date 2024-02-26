ab8478bbbab587047e070174d2e6d5434a47af08
sweta-singh28 <swetasinghgs28@gmail.com>
2024-02-27 02:00:57 +0530

    sweta

---
+++ Roller_Coaster.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x, h;
+	    cin>>x>>h;
+	    if(x>=h){
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
