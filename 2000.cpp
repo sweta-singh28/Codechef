b94709d9672bbbc6930749876d4a496194807692
sweta-singh28 <swetasinghgs28@gmail.com>
2024-02-27 01:54:23 +0530

    sweta

---
+++ 2000.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x, y;
+	cin>>x;
+	y = x * 4;
+	cout<<y<<endl;
+
+}
---
+++ Contests/Problem_Reviews.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	
+	while(T--){
+	    int N;
+	    cin>>N;
+	    if(N >= 4){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
