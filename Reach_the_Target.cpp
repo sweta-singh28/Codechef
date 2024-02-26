444dfbf17b4e65b0f3ce690b5e401b5f2a3c3bf1
sweta-singh28 <swetasinghgs28@gmail.com>
2024-02-26 09:56:30 +0530

    sweta

---
+++ Reach_the_Target.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+	while(T--){
+	    int x, y, z;
+	    cin>>x>>y;
+	    z = x - y;
+	    cout<<z<<endl;
+	    
+	}
+}
