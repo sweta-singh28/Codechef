a2a1fa2673e9e9b840790c190637c58edac10567
sweta-singh28 <swetasinghgs28@gmail.com>
2024-02-28 00:29:53 +0530

    sweta

---
+++ Chef_On_Date.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x, y;
+	    cin>>x>>y;
+	    if(x<y){
+	        cout<<"NO"<<endl;
+	    }
+	    else
+	    {
+	    cout<<"YES"<<endl;
+	    }
+	}
+
+}
