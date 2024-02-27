043166e8a31ba0c0b46e0a6185294ac4b3faefee
sweta-singh28 <swetasinghgs28@gmail.com>
2024-02-28 00:23:55 +0530

    sweta

---
+++ Candy_Division.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(n % 3 == 0){
+	    cout<<"YES"<<endl;
+	 }
+	else
+	{
+	    cout<<"NO"<<endl;
+	}
+
+}
+}
