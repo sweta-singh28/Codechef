90a510266a172d83cd73e6e6c9499a68d337bc31
sweta-singh28 <swetasinghgs28@gmail.com>
2024-02-28 00:27:57 +0530

    sweta

---
+++ Counting_Words.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int z, x, y;
+	    cin>>x>>y;
+	    z = x * y;
+	    cout<<z<<endl;
+	}
+
+}
