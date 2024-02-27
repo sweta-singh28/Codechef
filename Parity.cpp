7c9ca9c66087a2e23a154c2322b0b81da179ffae
sweta-singh28 <swetasinghgs28@gmail.com>
2024-02-28 00:32:12 +0530

    sweta

---
+++ Parity.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    if(n % 2 == 0){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"N0"<<endl;
+	    }
+	}
+
+}
