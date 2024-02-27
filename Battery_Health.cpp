21f61e9c49d14a9540f8e3f3905714afe4ab6eb3
sweta-singh28 <swetasinghgs28@gmail.com>
2024-02-28 00:26:17 +0530

    sweta

---
+++ Battery_Health.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x >= 80){
+	        cout<<"YES"<<endl;
+	    }
+	    else
+	    {
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
