e89a9a2e6ae78a146c16a70a6f5b8a15e96a53e5
sweta-singh28 <swetasinghgs28@gmail.com>
2024-02-29 22:28:56 +0530

    sweta

---
+++ Contests/Algomaniac_Finals.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	if(x==17){
+	    cout<<"YAY"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+
+}
