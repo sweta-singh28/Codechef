6d5f4017b8b9cccd7284ebe22f5409b03669c178
sweta-singh28 <swetasinghgs28@gmail.com>
2024-03-06 20:58:51 +0530

    sweta

---
+++ Chef_and_Donation.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x, y, z;
+	    cin>>x>>y;
+	    z = y - x;
+	    cout<<z<<endl;
+	}
+
+}
---
+++ Right_There.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x, n;
+	    cin>>n>>x;
+	    if(x>=n){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
