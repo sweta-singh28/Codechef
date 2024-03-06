5ab254912adb2b0c8d99c769efb5555abe2aa5f8
sweta-singh28 <swetasinghgs28@gmail.com>
2024-03-06 20:57:46 +0530

    sweta

---
+++ Chairs_Requirement.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x, y, z;
+	    cin>>x>>y;
+	    z = (x - y);
+	    if(z<0){
+	        cout<<0<<endl;
+	    }
+	    else{
+	        cout<<z<<endl;
+	    }
+	 
+	}
+
+}
