2b7474df37ba0f21d0e36c829612b6082c47a2da
sweta-singh28 <swetasinghgs28@gmail.com>
2024-03-08 21:05:19 +0530

    sweta

---
+++ Waiting_Time.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x, k, z;
+	    cin>>k>>x;
+	    z = 7 * k - x;
+	    cout<<z<<endl;
+	}
+
+}
