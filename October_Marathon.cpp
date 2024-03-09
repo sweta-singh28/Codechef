15efc4b9c518af406907876411e5493df2135a7f
sweta-singh28 <swetasinghgs28@gmail.com>
2024-03-09 13:07:39 +0530

    sweta

---
+++ October_Marathon.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	if(x<3){
+	    cout<<"GOLD";
+	}
+	else if(x>=3 && x<6){
+	    cout<<"SILVER";
+	}
+	else{
+	    cout<<"BRONZE";
+	}
+
+}
