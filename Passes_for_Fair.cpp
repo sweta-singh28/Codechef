04650ac4496681336279027d72ac99c8ce9f91cc
sweta-singh28 <swetasinghgs28@gmail.com>
2024-03-10 23:13:10 +0530

    sweta

---
+++ Passes_for_Fair.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+        int n, k;
+        cin>>n>>k;
+        if(k>n){
+            cout<<"YES"<<endl;
+        }
+        else{
+            cout<<"NO"<<endl;
+        }
+	}
+
+}
---
+++ Read_Pages.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n, x, y;
+	    cin>>n>>x>>y;
+	    if(n <= x * y){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	    
+	}
+
+}
