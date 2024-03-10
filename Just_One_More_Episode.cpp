f65f9e9dd9dd2b29ea61eacb2b4ef57f9be5d5d7
sweta-singh28 <swetasinghgs28@gmail.com>
2024-03-10 18:51:59 +0530

    sweta

---
+++ Just_One_More_Episode.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	    if(x>24){
+            cout<<"YES"<<endl;	        
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	    
+	}
+
+}
