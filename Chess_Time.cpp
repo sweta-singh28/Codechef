43693cdd946a286a1c364c1ded7118cdebe23cbf
sweta-singh28 <swetasinghgs28@gmail.com>
2024-03-10 23:00:58 +0530

    sweta

---
+++ Chess_Time.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n, m;
+	    cin>>n;
+	    m = (60 * n) / 20;
+	    cout<<m<<endl;
+	    
+	}
+
+}
