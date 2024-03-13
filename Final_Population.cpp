0027b24ae6bd0154f347e112ff742d9fe1e89c51
sweta-singh28 <swetasinghgs28@gmail.com>
2024-03-13 20:43:48 +0530

    sweta

---
+++ Final_Population.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x, y, z, a;
+	    cin>>x>>y>>z;
+	    a = x - y + z;
+	    cout<<a<<endl;
+	}
+
+}
