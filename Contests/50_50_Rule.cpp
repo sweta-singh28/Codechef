33b624be5a43ae19a6e25738e96169c392ab3c93
sweta-singh28 <swetasinghgs28@gmail.com>
2024-03-13 20:42:03 +0530

    sweta

---
+++ Contests/50_50_Rule.cpp
@@ -0,0 +1,23 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x, y;
+	    cin>>x>>y;
+	    if(x<50){
+	        cout<<"Z"<<endl;
+	    }
+	    else if(y<50 && x>=50){
+	        cout<<"F"<<endl;
+	    }
+	    else{
+	        cout<<"A"<<endl;
+	    }
+	    
+	}
+
+}
---
+++ Contests/Overspeeding.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int s;
+	cin>>s;
+	if(s>40){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+
+}
