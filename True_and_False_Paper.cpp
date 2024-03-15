b537050ad65eab9ccde0bbe9a2d96c5abaa31154
sweta-singh28 <swetasinghgs28@gmail.com>
2024-03-15 21:54:19 +0530

    sweta

---
+++ True_and_False_Paper.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n, k, z;
+	    cin>>n>>k;
+	    z = n - k;
+	    cout<<z<<endl;
+	    
+	}
+
+}
