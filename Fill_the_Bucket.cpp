db1aec342c999bc20f6f5b867aa6e52cf2e34967
sweta-singh28 <swetasinghgs28@gmail.com>
2024-03-16 23:05:24 +0530

    swewta

---
+++ Fill_the_Bucket.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int k, x;
+	    cin>>k>>x;
+	    cout<<k-x<<endl;
+	    
+	}
+
+}
