fed9c2f41c2301e4f81452f747d55681f45ce257
sweta-singh28 <swetasinghgs28@gmail.com>
2024-03-18 22:56:30 +0530

    sweta

---
+++ Chef_and_Masks.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x, y;
+	    cin>>x>>y;
+	    if(x*100<y*10){
+	        cout<<"Disposable"<<endl;
+	    }
+	    else{
+	        cout<<"Cloth"<<endl;
+	    }
+	    
+	    
+	}
+
+}
