1285dd792d4884e2457a964bdc0947e50c9c9832
sweta-singh28 <swetasinghgs28@gmail.com>
2024-03-25 23:03:21 +0530

    sweta

---
+++ Greater_Average.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int a,b,c;
+	    cin>>a>>b>>c;
+	    if((a+b)/2.0 > c){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
