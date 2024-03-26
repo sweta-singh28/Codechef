1551a428edaac32c09a0308e612e78804255230b
sweta-singh28 <swetasinghgs28@gmail.com>
2024-03-26 19:31:48 +0530

    heh

---
+++ Exams.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y,z;
+	    cin>>x>>y>>z;
+	    if((x*y)/2<z){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+
+}
+
