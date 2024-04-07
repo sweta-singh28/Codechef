9aaf127bb11db7f0329d9accfc3c3ebfc1d5e4de
sweta-singh28 <swetasinghgs28@gmail.com>
2024-04-07 22:08:41 +0530

    sweta

---
+++ Small_factorials.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n;
+	    cin>>n;
+	    int f = 1;
+	    for(int i=1; i<=n; i++)
+	      f*=i;
+	      {
+	          cout<<f<<endl;
+	       }
+	    
+	}
+	
+
+}
