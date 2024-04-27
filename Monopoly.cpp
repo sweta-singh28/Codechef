31273c8fd45f8f4ce15f20f86e161a7381093cb4
sweta-singh28 <swetasinghgs28@gmail.com>
2024-04-27 20:48:51 +0530

    sweta

--- Monopoly.cpp
+++ Monopoly.cpp
@@ -9,10 +9,10 @@ int main() {
 	    int p,q,r,s;
 	    cin>>p>>q>>r>>s;
 	    if(p>q+r+s || q>p+r+s || r>p+q+s || s>p+q+r){
-	        cout<<"YES"<<endl;
+	        cout<<"yES"<<endl;
 	    }
 	    else{
-	        cout<<"NO"<<endl;
+	        cout<<"nO"<<endl;
 	    }
 	}
 
