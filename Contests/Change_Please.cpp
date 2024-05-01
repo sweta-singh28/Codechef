a2765857c0516038848b3422668886343a3e6e92
sweta-singh28 <swetasinghgs28@gmail.com>
2024-05-01 20:31:00 +0530

    sweta

---
+++ Contests/Change_Please.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x;
+	    cin>>x;
+	 	cout<<((100-x)/10)*10<<endl;
+	 
+	    
+	}
+}
