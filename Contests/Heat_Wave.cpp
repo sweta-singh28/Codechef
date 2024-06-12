ab44ce67cfcd3c4e3fe3b256926314555bea9b4c
sweta-singh28 <swetasinghgs28@gmail.com>
2024-06-12 20:28:26 +0530

    contest

---
+++ Contests/Heat_Wave.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y;
+	cin>>x>>y;
+	if(x>=y){
+	    cout<<"NO"<<endl;
+	}
+	else{
+	    cout<<"YES"<<endl;
+	}
+
+}
