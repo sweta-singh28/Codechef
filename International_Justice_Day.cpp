9c9bd5cb5a92d3a91566fb8016aa165e1c857c57
sweta-singh28 <swetasinghgs28@gmail.com>
2024-07-26 21:16:24 +0530

    by sweta

---
+++ International_Justice_Day.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x,y;
+	cin>>x>>y;
+	if(x>=y){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+	
+	
+
+}
