2c93d17c424bd56ff064d5285a5f008134cf3058
sweta-singh28 <swetasinghgs28@gmail.com>
2024-10-30 00:19:54 +0530

    sweta

---
+++ IOI_2024.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	if(x>=1 && x<=8){
+	    cout<<"YES"<<endl;
+	}
+	else{
+	    cout<<"NO"<<endl;
+	}
+
+}
---
+++ Lucky_Clover.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n;
+	cin>>n;
+	cout<<(n-1)*3 + (n-(n-1)) * 4<<endl;
+
+}
