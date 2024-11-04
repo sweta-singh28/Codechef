c3df3cc08a2a431cd37cbb3923b9070065a31e88
sweta-singh28 <swetasinghgs28@gmail.com>
2024-11-04 21:00:30 +0530

    by sweta

---
+++ Enough_Space.cpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int n,x,y;
+	    cin>>n>>x>>y;
+	    if(x*1+y*2<=n){
+	        cout<<"YES"<<endl;
+	    }
+	    else{
+	        cout<<"NO"<<endl;
+	    }
+	}
+	
+
+}
---
+++ Football_Cup.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int t;
+	cin>>t;
+	while(t--){
+	    int x,y;
+	    cin>>x>>y;
+	    if(x!=y || x==0 || y==0){
+	        cout<<"NO"<<endl;
+	    }
+	    else{
+	        cout<<"YES"<<endl;
+	    }
+	}
+
+}
