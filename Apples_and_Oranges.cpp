7ca7f15b88cc75a8ad335126ba0db7d2011910ae
sweta-singh28 <swetasinghgs28@gmail.com>
2024-11-04 20:59:45 +0530

    hm

---
+++ Apples_and_Oranges.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int x;
+	cin>>x;
+	int a,b;
+	cin>>a>>b;
+	if(x>=a+b){
+	    cout<<"Yes"<<endl;
+	}
+	else{
+	    cout<<"No"<<endl;
+	}
+
+}
