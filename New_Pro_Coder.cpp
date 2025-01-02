a6f21604f46e3b4b62f599a7382e6e9f91804b99
sweta-singh28 <swetasinghgs28@gmail.com>
2025-01-02 13:46:16 +0530

    sweta

---
+++ New_Pro_Coder.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	float n,m;
+	cin>>n>>m;
+	if(m>=n/2){
+	    cout<<"NEWBIE"<<endl;
+	}
+	else{
+	    cout<<"PRO"<<endl;
+	}
+
+}
