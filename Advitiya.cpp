cef9c2d53fe7cd96dbd54ea221f4cbb61decd5ee
sweta-singh28 <swetasinghgs28@gmail.com>
2025-01-29 20:16:31 +0530

    contest

--- Advitiya.cpp
+++ Advitiya.cpp
@@ -1,15 +1,36 @@
-#include <bits/stdc++.h>
+#include <iostream>
+#include <cmath>
 using namespace std;
 
-int main() {
-	// your code goes here
-	int n;
-	cin>>n;
-	if(n == 16 || n == 17 || n==18){
-	    cout<<"ADVITIYA"<<endl;
-	}
-	else{
-	    cout<<"WAITING FOR ADVITIYA"<<endl;
-	}
+int minCharDistance(char from, char to) {
+    if (from <= to) {
+        return to - from;  // Normal forward distance
+    } else {
+        return (26 - (from - to));  // Wrap-around distance
+    }
+}
 
+void solve() {
+    string str;
+    cin >> str;
+    
+    string target = "ADVITIYA";
+    int totalCost = 0;
+
+    for (int i = 0; i < target.length(); i++) {
+        totalCost += minCharDistance(str[i], target[i]);
+    }
+
+    cout << totalCost << endl;
+}
+
+int main() {
+    int t;
+    cin >> t;
+    
+    while (t--) {
+        solve();
+    }
+
+    return 0;
 }
---
+++ Squid_Game_Piggy_Bank.cpp
@@ -0,0 +1,10 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+	int n,k;
+	cin>>n>>k;
+	cout<<(n-k )*10000<<endl;
+
+}
